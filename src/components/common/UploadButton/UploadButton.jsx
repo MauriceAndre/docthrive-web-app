e682ac4047b9ea90f5ec94bb598462461f43c3b3
Maurice Schmid <maurice.schmid@live.de>
2020-04-27 18:19:53 -0700

    Add UploadButton comp.

---
+++ src/components/common/UploadButton/UploadButton.jsx
@@ -0,0 +1,64 @@
+import React, { useMemo, Fragment } from "react";
+import { connect } from "react-redux";
+import * as actionCreators from "./../../../store/actions/index";
+import { useDropzone } from "react-dropzone";
+import { join } from "./../../../utils/arrayUtils";
+import { initT, useT, t } from "../../../utils/intl";
+import { uploadDocuments } from "../../ModalHandler/actions";
+import style from "./UploadButton.module.css";
+
+function UploadButton({ setModal, showModal }) {
+  initT(useT(), "uploadButton");
+
+  const onDrop = (files) => {
+    console.log(files);
+    setModal(uploadDocuments(showModal, files));
+  };
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({ onDrop });
+
+  const dropzoneStyle = useMemo(
+    () =>
+      (isDragAccept && style["drag-accept"]) ||
+      (isDragReject && style["drag-reject"]),
+    [isDragAccept, isDragReject]
+  );
+
+  return (
+    <Fragment>
+      {/* <ProgressBar
+        variant="primary"
+        animated
+        now={90}
+        label={`${90}%`}
+        style={{ height: "2rem" }}
+      /> */}
+      <div
+        {...getRootProps({
+          className: join([
+            "btn btn-sm btn-outline-primary",
+            style["upload-btn"],
+            dropzoneStyle,
+          ]),
+        })}
+      >
+        <input {...getInputProps()} />
+        <span>{t("label")}</span>
+      </div>
+    </Fragment>
+  );
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setModal: (modal) => dispatch(actionCreators.setModal(modal)),
+    showModal: (show) => dispatch(actionCreators.showModal(show)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(UploadButton);
