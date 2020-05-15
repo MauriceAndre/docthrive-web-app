14a13528f3025c542db9ed027160aaec64c1c442
Maurice Schmid <maurice.schmid@live.de>
2020-05-14 23:51:29 -0700

    Enhance ViewSidebar comp.

--- src/components/Archive/components/ViewSidebar/ViewSidebar.jsx
+++ src/components/Archive/components/ViewSidebar/ViewSidebar.jsx
@@ -1,66 +1,95 @@
-import React from "react";
+import React, { Fragment } from "react";
+import useViews from "./useViews";
 import { Accordion, Card, ListGroup, Container } from "react-bootstrap";
-import TreeView from "./../TreeView";
 import Icon from "./../../../common/Icon";
-import { initT, t, useT } from "../../../../utils/intl";
+import { t } from "../../../../utils/intl";
 import { join } from "./../../../../utils/arrayUtils";
+import config from "./../../../../services/configService";
 import style from "./ViewSidebar.module.css";
 
+const viewSidebarConf = config.default.viewSidebar;
+const viewsKey = viewSidebarConf.viewsKey;
+const defaultView = viewSidebarConf.view;
+
 const ViewSidebar = (props) => {
-  initT(useT(), "archivSidebar");
-  return (
+  const { views, view, setView, activeKey, setActiveKey } = useViews(
+    defaultView
+  );
+
+  const onToggle = (key) => setActiveKey(key);
+
+  const renderView = ({ icon, text, key, content }) => {
+    const Component = content.component;
+
+    return (
+      <Fragment>
+        <Accordion.Toggle
+          as={Card.Header}
+          eventKey={key}
+          className="bg-dark text-white p-2"
+          onClick={() => onToggle(key)}
+        >
+          <Icon name={icon} text={text} />
+        </Accordion.Toggle>
+        <Accordion.Collapse
+          eventKey={key}
+          className="section-wrapper section-fill"
+        >
+          <Card.Body
+            className={[
+              "overflow-auto section-content p-2",
+              style["view-body"],
+            ]}
+          >
+            {Component ? <Component /> : null}
+          </Card.Body>
+        </Accordion.Collapse>
+      </Fragment>
+    );
+  };
+
+  return view ? (
     <Container className="section-content p-3">
       <Accordion
-        defaultActiveKey="archive"
+        activeKey={activeKey}
+        defaultActiveKey={view.key}
         className={join(["section section-column", style.accordion])}
       >
         <Card>
           <Accordion.Toggle
             as={Card.Header}
-            eventKey="views"
+            eventKey={viewsKey}
             className="bg-dark text-white p-2"
+            onClick={() => onToggle(viewsKey)}
           >
             {t("tabs.views")}
           </Accordion.Toggle>
-          <Accordion.Collapse eventKey="views">
+          <Accordion.Collapse eventKey={viewsKey}>
             <ListGroup variant="flush">
-              <ListGroup.Item action>
-                <Icon name="tags" text={t("views.labels")} />
-              </ListGroup.Item>
-              <ListGroup.Item action>
-                <Icon name="folder" text={t("views.tree")} />
-              </ListGroup.Item>
-              <ListGroup.Item action>
-                <Icon name="calendar-alt" text={t("views.date")} />
-              </ListGroup.Item>
+              {views.map((view) => {
+                const { key, icon, text } = view;
+                return (
+                  <ListGroup.Item
+                    key={key}
+                    action
+                    onClick={() => {
+                      setView(view);
+                      setActiveKey(key);
+                    }}
+                  >
+                    <Icon name={icon} text={text} />
+                  </ListGroup.Item>
+                );
+              })}
             </ListGroup>
           </Accordion.Collapse>
         </Card>
         <Card className="section-wrapper section-fill section section-column">
-          <Accordion.Toggle
-            as={Card.Header}
-            eventKey="archive"
-            className="bg-dark text-white p-2"
-          >
-            {t("views.tree")}
-          </Accordion.Toggle>
-          <Accordion.Collapse
-            eventKey="archive"
-            className="section-wrapper section-fill"
-          >
-            <Card.Body
-              className={[
-                "overflow-auto section-content p-2",
-                style["view-body"],
-              ]}
-            >
-              <TreeView />
-            </Card.Body>
-          </Accordion.Collapse>
+          {renderView(view)}
         </Card>
       </Accordion>
     </Container>
-  );
+  ) : null;
 };
 
 export default ViewSidebar;
---
+++ src/components/Archive/components/ViewSidebar/useViews.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect, useMemo } from "react";
+import TreeView from "./../TreeView";
+import { initT, t, useT } from "../../../../utils/intl";
+
+export default function (defaultView) {
+  const [view, setView] = useState();
+  const [activeKey, setActiveKey] = useState();
+  initT(useT(), "archivSidebar");
+
+  const views = useMemo(
+    () => [
+      {
+        key: "labels",
+        text: t("views.labels"),
+        icon: "tags",
+        content: {},
+      },
+      {
+        key: "tree",
+        text: t("views.tree"),
+        icon: "folder",
+        content: {
+          component: TreeView,
+        },
+      },
+      {
+        key: "date",
+        text: t("views.date"),
+        icon: "calendar-alt",
+        content: {},
+      },
+    ],
+    []
+  );
+
+  useEffect(() => {
+    setView(views.find(({ key }) => key === defaultView));
+  }, [views, defaultView]);
+
+  return {
+    views,
+    view,
+    setView,
+    activeKey,
+    setActiveKey,
+  };
+}
