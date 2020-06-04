6974731a37e56c31cc01d2416205cda9741ce054
Maurice Schmid <maurice.schmid@live.de>
2020-06-04 01:57:14 -0700

    Add Home comp.

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { connect } from "react-redux";
+import LoginForm from "../common/LoginForm";
+import RegisterForm from "../common/RegisterForm";
+
+function Home({ user }) {
+  if (user) return (window.location = "/archive");
+
+  return (
+    <div className="section-content overflow-auto">
+      <h1 className="text-center display-3">DocThrive</h1>
+      <div className="d-flex flex-column flex-md-row justify-content-around align-items-center">
+        <div className="w-100 m-4 p-4 border rounded bg-light">
+          <h1 className="text-center">Login</h1>
+          <LoginForm />
+        </div>
+        <strong className="m-3">OR</strong>
+        <div className="w-100 m-4 p-4 border rounded bg-light">
+          <h1 className="text-center">Register</h1>
+          <RegisterForm />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = ({ user }) => {
+  return {
+    user: user.user,
+  };
+};
+
+export default connect(mapStateToProps)(Home);
--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Switch, Redirect } from "react-router-dom";
 import Route from "./Route";
+import ProtectedRoute from "./ProtectedRoute";
 import NotFound from "../NotFound/NotFound";
 import Archive from "../Archive/Archive";
 import Register from "./../Register";
 import Login from "./../Login";
 import Logout from "./../Logout";
-import ProtectedRoute from "./ProtectedRoute";
 import MainNavbar from "./../MainNavbar/MainNavbar";
+import Home from "./../Home";
 
 const Routes = () => {
   return (
@@ -22,7 +23,7 @@ const Routes = () => {
       <Route path="/login" component={Login} />
       <Route path="/logout" component={Logout} />
       <Route path="/not-found" component={NotFound} />
-      <Redirect exact path="/" to="/archive" />
+      <Route exact path="/" component={Home} />
       <Redirect to="/not-found" />
     </Switch>
   );
