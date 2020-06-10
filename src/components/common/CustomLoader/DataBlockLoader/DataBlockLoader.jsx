2e3e9623c98b69f4437a79dd15aacf1332af0198
Maurice Schmid <maurice.schmid@live.de>
2020-06-10 16:34:58 -0700

    Add loader for ElementActivity

---
+++ src/components/common/CustomLoader/DataBlockLoader/DataBlockLoader.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const MyLoader = (props) => (
+  <ContentLoader
+    speed={2}
+    width={"100%"}
+    height={320}
+    viewBox="0 0 370 320"
+    backgroundColor="#d9d9d9"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <rect x="5" y="9" rx="6" ry="6" width="357" height="93" />
+    <rect x="5" y="113" rx="6" ry="6" width="357" height="93" />
+    <rect x="5" y="216" rx="6" ry="6" width="357" height="93" />
+  </ContentLoader>
+);
+
+export default MyLoader;
